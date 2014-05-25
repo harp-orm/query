f15a236310b6dd7e23581dc382600c8a0f455189
Ivan Kerin <ikerin@gmail.com>
2014-05-25 13:52:00 +0300

    :lipstick: Disable prepared statement emulation by default
    This way PDO uses native mysql driver, for less memory

--- src/DB.php
+++ src/DB.php
@@ -35,6 +35,7 @@ class DB extends PDO
         'driverOptions' => array(
             PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
             PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+            PDO::ATTR_EMULATE_PREPARES => false,
         ),
         'logger' => null,
     );
